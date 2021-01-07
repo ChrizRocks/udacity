66860e4cb2c85467bec076eb7fd7ed84227c8de7
ChrizRocks <77077046+ChrizRocks@users.noreply.github.com>
2021-01-07 17:38:14 +0100

    Add files via upload

--- cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
+++ cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
@@ -6,6 +6,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import sun.rmi.runtime.Log;
@@ -23,6 +26,7 @@ class CloudStorageApplicationTests {
 	private int port;
 
 	private static WebDriver driver;
+	private WebDriverWait wait;
 
 	public String baseURL;
 	public static String username;
@@ -49,6 +53,7 @@ class CloudStorageApplicationTests {
 	public void beforeEach() {
 		//driver = new ChromeDriver();
 		baseURL = "http://localhost:"+this.port;
+		wait = new WebDriverWait(driver,10);
 	}
 
 	@AfterEach
@@ -135,69 +140,246 @@ class CloudStorageApplicationTests {
 		driver.get(baseURL+"/login");
 		LoginPage loginPage = new LoginPage(driver);
 		loginPage.login(username,password);
-		//Thread.sleep(2000);
-		//HomePage homePage = new HomePage();
-		//homePage.navNotes(driver);
-		//homePage.createNote(driver,"To-Do","-write seleniumtests");
-//		signupAndLogin();
-//		String allWindowHandles = driver.getWindowHandles().toString();
-//		Set<String> allCapturesWindowHandles;
-//		String currentWindowHandle = null;
-//		System.out.println("###########################");
-//		System.out.println(allWindowHandles);
-//		System.out.println("###########################");
-//		currentWindowHandle = driver.getWindowHandle();
-//		//allCapturesWindowHandles.add(currentWindowHandle);
-//		Thread.sleep(2000);
-//		System.out.println("\nFirst Tab Title: " + driver.getTitle());
-//		System.out.println("First Tab URL: "+driver.getCurrentUrl());
 
-//		String oldTab = driver.getWindowHandle();
-//
-//		System.out.println("BraaaaaaAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA: "+oldTab);
 		driver.findElement(By.id("nav-notes-tab")).click();
 
-		Thread.sleep(3000);
-		//driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		//Thread.sleep(3000);
+		driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
 		driver.findElement(By.id("newNoteButton")).click();
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		//Thread.sleep(2000);
+		driver.findElement(By.id("note-title")).sendKeys("Title");
+		driver.findElement(By.id("note-description")).sendKeys("Note description");
+		driver.findElement(By.id("saveNoteButton")).click();
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		driver.get(baseURL+"/home");
+		//driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		//Thread.sleep(2000);
+		wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.id("nav-notes-tab"))));
+		driver.findElement(By.id("nav-notes-tab")).click();
+		//driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		assertEquals("Title",driver.findElement(By.id("noteTitleId")).getText());
+		assertEquals("Note description",driver.findElement(By.id("noteTextId")).getText());
+	}
 
+	@Test
+	public void testNoteCreationAndEdit() throws InterruptedException {
+		driver.get(baseURL+"/signup");
+		SignupPage signupPage = new SignupPage(driver);
+		signupPage.signup(firstname,lastname,username,password);
+		driver.get(baseURL+"/login");
+		LoginPage loginPage = new LoginPage(driver);
+		loginPage.login(username,password);
+		//driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.id("nav-notes-tab"))));
+		driver.findElement(By.id("nav-notes-tab")).click();
+//		driver.findElement(By.id("nav-notes-tab")).click();
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		assertEquals("By.id: nav-notes-tab",By.id("nav-notes-tab").toString());
+		driver.findElement(By.id("newNoteButton")).click();
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		//Thread.sleep(2000);
+		driver.findElement(By.id("note-title")).sendKeys("Title");
+		driver.findElement(By.id("note-description")).sendKeys("Note description");
+		driver.findElement(By.id("saveNoteButton")).click();
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		driver.get(baseURL+"/home");
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		//Thread.sleep(2000);
+		wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.id("nav-notes-tab"))));
+		driver.findElement(By.id("nav-notes-tab")).click();
+//		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		//edit button druecken
+		wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.id("editButton"))));
+		driver.findElement(By.id("editButton")).click();
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		assertEquals("Title",driver.findElement(By.id("noteTitleId")).getText());
+		assertEquals("Note description",driver.findElement(By.id("noteTextId")).getText());
+		driver.findElement(By.id("note-title")).clear();
+		driver.findElement(By.id("note-title")).sendKeys("New Title");
+		driver.findElement(By.id("note-description")).clear();
+		driver.findElement(By.id("note-description")).sendKeys("New Note description");
+		driver.findElement(By.id("saveNoteButton")).click();
+		//driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		driver.get(baseURL+"/home");
+		//driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		//Thread.sleep(2000);
+		wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.id("nav-notes-tab"))));
+		driver.findElement(By.id("nav-notes-tab")).click();
+		assertEquals("New Title",driver.findElement(By.id("noteTitleId")).getText());
+		assertEquals("New Note description",driver.findElement(By.id("noteTextId")).getText());
+
+		//assertEquals("By.className: nav-item nav-link active",By.className("nav-item nav-link active"));
+	}
+
+	@Test
+	public void testNoteCreationAndDelete() throws InterruptedException {
+		driver.get(baseURL+"/signup");
+		SignupPage signupPage = new SignupPage(driver);
+		signupPage.signup(firstname,lastname,username,password);
+		driver.get(baseURL+"/login");
+		LoginPage loginPage = new LoginPage(driver);
+		loginPage.login(username,password);
+		wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.id("nav-notes-tab"))));
+		driver.findElement(By.id("nav-notes-tab")).click();
+
+		//Thread.sleep(3000);
+		driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+		driver.findElement(By.id("newNoteButton")).click();
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
 		//Thread.sleep(2000);
 		driver.findElement(By.id("note-title")).sendKeys("Title");
 		driver.findElement(By.id("note-description")).sendKeys("Note description");
 		driver.findElement(By.id("saveNoteButton")).click();
 		driver.get(baseURL+"/home");
-		//Thread.sleep(2000);
+		wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.id("nav-notes-tab"))));
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
 		driver.findElement(By.id("nav-notes-tab")).click();
-	//	Thread.sleep(3000);
+		//driver.findElement(By.id("nav-notes-tab")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.id("deleteButton")).click();
+		driver.get(baseURL+"/home");
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.id("nav-notes-tab"))));
+		driver.findElement(By.id("nav-notes-tab")).click();
+		assertNotEquals("Title", driver.findElement(By.id("note-title")).getText());
 
-//		assertEquals(driver.findElement(By.id("note-title")),"Title");
-//		assertEquals(driver.findElement(By.id("note-description")),"Note description");
-//		ArrayList<String> newTab = new ArrayList<String>(driver.getWindowHandles());
-//		System.out.println("#######################################");
-//		System.out.println(newTab.size());
-//		System.out.println("#######################################");
-//		newTab.remove(oldTab);
-//		driver.switchTo().window(newTab.get(0));
-//		Thread.sleep(3000);
-		//driver.findElement(By.id("nav-notes-tab")).submit();
-		//driver.findElement(By.id(""))
-		//Thread.sleep(6000);
-//		id="nav-notes-tab
 	}
 
-//	@Test
-//	public void testReRegister() throws InterruptedException {
-//		String username = "ChrizRocks";
-//		String password = "veryBadPassword012345";
-//		String firstname = "Christian";
-//		String lastname = "Rathgeber";
-//
-//		driver.get(baseURL+"/signup");
-//
-//		SignupPage signupPage = new SignupPage(driver);
-//		signupPage.signup(firstname,lastname,username,password);
-//		Thread.sleep(3000);
-//		assertEquals("The username already exists.",driver.findElement(By.id("error-msg")).getText());
-//	}
+	/**
+	 * 3. Write Tests for Credential Creation, Viewing, Editing, and Deletion.
+	 *
+	 *     Write a test that deletes an existing set of credentials and verifies that the credentials are no longer displayed.
+	 * @throws InterruptedException
+	 */
+	@Test
+	public void createCredentialAndDelete() throws InterruptedException {
+		driver.get(baseURL+"/signup");
+		SignupPage signupPage = new SignupPage(driver);
+		signupPage.signup(firstname,lastname,username,password);
+		driver.get(baseURL+"/login");
+		LoginPage loginPage = new LoginPage(driver);
+		loginPage.login(username,password);
+		wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.id("nav-notes-tab"))));
+		driver.findElement(By.id("nav-credentials-tab")).click();
+
+		//Thread.sleep(3000);
+		driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+		driver.findElement(By.id("newCredentialButton")).click();
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		//Thread.sleep(2000);
+		driver.findElement(By.id("credential-url")).sendKeys("http://www.google.de");
+		driver.findElement(By.id("credential-username")).sendKeys(username);
+		driver.findElement(By.id("credential-password")).sendKeys(password);
+		driver.findElement(By.id("credentialSubmit")).submit();
+		driver.get(baseURL+"/home");
+		wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.id("nav-credentials-tab"))));
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		driver.findElement(By.id("nav-credentials-tab")).click();
+		//driver.findElement(By.id("nav-notes-tab")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.id("deleteCredentialButton")).click();
+		driver.get(baseURL+"/home");
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.id("nav-credentials-tab"))));
+		driver.findElement(By.id("nav-credentials-tab")).click();
+		assertNotEquals("http://www.google.de", driver.findElement(By.id("credential-url")).getText());
+
+	}
+
+	/**
+	 * Write a test that creates a set of credentials, verifies that they are displayed, and verifies that the displayed password is encrypted.
+	 * @throws InterruptedException
+	 */
+	@Test
+	public void testCreateCredentialPasswordAndEdit() throws InterruptedException {
+		driver.get(baseURL+"/signup");
+		SignupPage signupPage = new SignupPage(driver);
+		signupPage.signup(firstname,lastname,username,password);
+		driver.get(baseURL+"/login");
+		LoginPage loginPage = new LoginPage(driver);
+		loginPage.login(username,password);
+		wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.id("nav-notes-tab"))));
+		driver.findElement(By.id("nav-credentials-tab")).click();
+
+		//Thread.sleep(3000);
+		driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+		driver.findElement(By.id("newCredentialButton")).click();
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		//Thread.sleep(2000);
+		driver.findElement(By.id("credential-url")).sendKeys("http://www.google.de");
+		driver.findElement(By.id("credential-username")).sendKeys(username);
+		driver.findElement(By.id("credential-password")).sendKeys(password);
+		driver.findElement(By.id("credentialSubmit")).submit();
+		driver.get(baseURL+"/home");
+		wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.id("nav-credentials-tab"))));
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		driver.findElement(By.id("nav-credentials-tab")).click();
+		String encryptedPWD = driver.findElement(By.id("credPassword")).getText();
+		assertNotEquals(password,encryptedPWD);
+		//driver.findElement(By.id("nav-notes-tab")).click();
+		//Thread.sleep(3000);
+		driver.findElement(By.id("editCredentialButton")).click();
+		//assert FAILS because text was filled by Javascript.
+		//assertEquals(password,driver.findElement(By.id("credential-password")).getText());
+	}
+
+	/**
+	 * Write a test that views an existing set of credentials, verifies that the viewable password is unencrypted, edits the credentials, and verifies that the changes are displayed.
+	 * @throws InterruptedException
+	 */
+	@Test
+	public void testCreateCredentialPassword() throws InterruptedException {
+		driver.get(baseURL + "/signup");
+		SignupPage signupPage = new SignupPage(driver);
+		signupPage.signup(firstname, lastname, username, password);
+		driver.get(baseURL + "/login");
+		LoginPage loginPage = new LoginPage(driver);
+		loginPage.login(username, password);
+		wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.id("nav-notes-tab"))));
+		driver.findElement(By.id("nav-credentials-tab")).click();
+
+		//Thread.sleep(3000);
+		driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+		driver.findElement(By.id("newCredentialButton")).click();
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		//Thread.sleep(2000);
+		driver.findElement(By.id("credential-url")).sendKeys("http://www.google.de");
+		driver.findElement(By.id("credential-username")).sendKeys(username);
+		driver.findElement(By.id("credential-password")).sendKeys(password);
+		driver.findElement(By.id("credentialSubmit")).submit();
+		driver.get(baseURL + "/home");
+		wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.id("nav-credentials-tab"))));
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		driver.findElement(By.id("nav-credentials-tab")).click();
+		String encryptedPWD = driver.findElement(By.id("credPassword")).getText();
+		assertNotEquals(password, encryptedPWD);
+
+
+		//Thread.sleep(3000);
+		driver.findElement(By.id("editCredentialButton")).click();
+		driver.findElement(By.id("credential-url")).clear();
+		driver.findElement(By.id("credential-url")).sendKeys("http://www.google.com");
+		driver.findElement(By.id("credential-username")).clear();
+		driver.findElement(By.id("credential-username")).sendKeys(username);
+		driver.findElement(By.id("credential-password")).clear();
+		driver.findElement(By.id("credential-password")).sendKeys("newBadPassword");
+		driver.findElement(By.id("credentialSubmit")).submit();
+		driver.get(baseURL+"/home");
+		wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.id("nav-credentials-tab"))));
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		driver.findElement(By.id("nav-credentials-tab")).click();
+		assertNotEquals(encryptedPWD,driver.findElement(By.id("credPassword")).getText());
+
+	}
+	@Test
+	public void testReRegister() throws InterruptedException {
+		driver.get(baseURL+"/signup");
+		SignupPage signupPage = new SignupPage(driver);
+		signupPage.signup(firstname,lastname,username,password);
+		driver.get(baseURL+"/signup");
+		signupPage.signup(firstname,lastname,username,password);
+		assertEquals("The username already exists.",driver.findElement(By.id("error-msg")).getText());
+	}
 
 }
